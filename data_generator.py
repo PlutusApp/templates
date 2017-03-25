57f1492dfe828d2b26b36fca14d7d65ab6829e95
Vishnu Vijayan <vvijayan@purdue.edu>
2017-03-25 05:06:05 -0400

    Update dataset generator

--- data_generator.py
+++ data_generator.py
@@ -7,7 +7,7 @@ def poor_big_spender():
     total_income = random.randint(6000, 40000)+random.randint(-1, 1)*random.randint(1000, 3000)
     monthly_income = total_income/12.0
     rcs = -0.6
-    for i in range(0,12):
+    for i in range(1,12):
         poor_status = random.uniform(0, 0.5)
         saving_rate = random.uniform(0,3)
         spending_rate = random.uniform(7,10)
@@ -25,7 +25,7 @@ def saver_big_spender():
     total_income = random.randint(6000, 200000)+random.randint(-1, 1)*random.randint(1000, 3000)
     monthly_income = total_income/12.0
     rcs = -0.6
-    for i in range(0,12):
+    for i in range(1,12):
         poor_status = random.uniform(0, 1)
         saving_rate = random.uniform(7,10)
         spending_rate = random.uniform(7,10)
@@ -43,7 +43,7 @@ def saver_small_spender():
     total_income = random.randint(6000, 200000)+random.randint(-1, 1)*random.randint(1000, 3000)
     monthly_income = total_income/12.0
     rcs = -0.6
-    for i in range(0,12):
+    for i in range(1,12):
         poor_status = random.uniform(0, 1)
         saving_rate = random.uniform(7,10)
         spending_rate = random.uniform(0,3)
@@ -60,7 +60,7 @@ def average_spender():
     total_income = random.randint(6000, 200000)+random.randint(-1, 1)*random.randint(1000, 3000)
     monthly_income = total_income/12.0
     rcs = -0.6
-    for i in range(0,12):
+    for i in range(1,12):
         poor_status = random.uniform(0, 1)
         saving_rate = random.uniform(4,7)
         spending_rate = random.uniform(4,7)
@@ -74,8 +74,8 @@ def average_spender():
 if __name__ == "__main__":
     data = []
     targets = []
-    for i in range(0, 1000000000):
-        rand = random.randint(0, 4)
+    for i in range(0, 1000000):
+        rand = random.randint(0, 3)
         targets.append(rand)
         if(rand == 0): data.append(poor_big_spender())
         elif (rand == 1): data.append(saver_big_spender())
