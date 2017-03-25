da45ae73658a5b0e4a65a2b2d4055f0f86de7f12
Vishnu Vijayan <vvijayan@purdue.edu>
2017-03-25 04:01:00 -0400

    Update dataset generator

--- data_generator.py
+++ data_generator.py
@@ -12,11 +12,31 @@ def poor_big_spender():
         spending_rate = random.uniform(7,10)
         monthly_expenditure = monthly_income/saving_rate
         account_total = total_income*(1.0-poor_status)
-        rcs = (monthly_income/monthly_expenditure)
+        rcs = (monthly_income-monthly_expenditure)/monthly_income
         data = [x + y for x, y in zip(data, [monthly_income, monthly_expenditure, total_income, rcs, age, account_total])]
     data = [x / 12 for x in data]
     return data
 
+
+def saver_big_spender():
+    data = [0,0,0,0,0,0]
+    age = random.randint(16, 80)
+    total_income = random.randint(6000, 200000)+random.randint(-1, 1)*random.randint(1000, 3000)
+    monthly_income = total_income/12.0
+    rcs = -0.6
+    for i in range(0,12):
+        poor_status = random.uniform(0, 1)
+        saving_rate = random.uniform(7,10)
+        spending_rate = random.uniform(7,10)
+        monthly_expenditure = monthly_income/saving_rate
+        account_total = total_income*(1.0-poor_status)
+        rcs = (monthly_income-monthly_expenditure)/monthly_income
+        data = [x + y for x, y in zip(data, [monthly_income, monthly_expenditure, total_income, rcs, age, account_total])]
+    data = [x / 12 for x in data]
+    return data
+
+
+
 if __name__ == "__main__":
     for i in range(0, 100):
-        print(poor_big_spender())
+        print(saver_big_spender())
